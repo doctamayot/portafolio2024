25da0cea6c78cd65df56a9d9f00f792cc763f3eb
Hugo Tamayo <doctamayot@hotmail.com>
2023-12-10 18:40:40 -0500

    intento 1

--- app/page.tsx
+++ app/page.tsx
@@ -67,12 +67,12 @@ export default async function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header data={socials?.socials} />
-      <section id="hero" className="snap-start">
+      {/* <section id="hero" className="snap-start">
         <Hero data={pageInfo} />
-      </section>
-      <section id="about" className="snap-center">
+      </section> */}
+      {/* <section id="about" className="snap-center">
         <About data={pageInfo} />
-      </section>
+      </section> */}
       {/* <section id="experience" className="snap-center">
         <Experience data={expInfo?.experiences} />
       </section> */}
