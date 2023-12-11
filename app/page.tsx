7a4f7c2025b8e8b03a3971fa01f3696b74d9f8d2
Hugo Tamayo <doctamayot@hotmail.com>
2023-12-10 19:12:20 -0500

    intento 1

--- app/page.tsx
+++ app/page.tsx
@@ -42,45 +42,46 @@ async function getSocials() {
   });
   const socials = await resSocial.json();
 
-  // const resPage = await fetch(`${BASEURL}/api/pageinfo`, {
-  //   next: { revalidate: 60 },
-  // });
-  // const pageInfo = await resPage.json();
-  // const resExp = await fetch(`${BASEURL}/api/experience`, {
-  //   next: { revalidate: 60 },
-  // });
-  // const expInfo = await resExp.json();
-  // const resSkills = await fetch(`${BASEURL}/api/skills`, {
-  //   next: { revalidate: 60 },
-  // });
-  // const skillsInfo = await resSkills.json();
-  // const resProjects = await fetch(`${BASEURL}/api/projects`, {
-  //   next: { revalidate: 60 },
-  // });
-  // const projectsInfo = await resProjects.json();
-  return { socials };
+  const resPage = await fetch(`${baseUrl}/api/pageinfo`, {
+    next: { revalidate: 60 },
+  });
+  const pageInfo = await resPage.json();
+  const resExp = await fetch(`${baseUrl}/api/experience`, {
+    next: { revalidate: 60 },
+  });
+  const expInfo = await resExp.json();
+  const resSkills = await fetch(`${baseUrl}/api/skills`, {
+    next: { revalidate: 60 },
+  });
+  const skillsInfo = await resSkills.json();
+  const resProjects = await fetch(`${baseUrl}/api/projects`, {
+    next: { revalidate: 60 },
+  });
+  const projectsInfo = await resProjects.json();
+  return { socials, pageInfo, expInfo, skillsInfo, projectsInfo };
 }
 
 export default async function Home() {
-  const { socials } = await getSocials();
+  const { socials, pageInfo, expInfo, skillsInfo, projectsInfo } =
+    await getSocials();
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Header data={socials?.socials} />
-      {/* <section id="hero" className="snap-start">
+      <section id="hero" className="snap-start">
         <Hero data={pageInfo} />
-      </section> */}
-      {/* <section id="about" className="snap-center">
+      </section>
+      <section id="about" className="snap-center">
         <About data={pageInfo} />
-      </section> */}
-      {/* <section id="experience" className="snap-center">
+      </section>
+      <section id="experience" className="snap-center">
         <Experience data={expInfo?.experiences} />
-      </section> */}
-      {/* <section id="skills" className="snap-center">
+      </section>
+      <section id="skills" className="snap-center">
         <Skills data={skillsInfo?.skills} />
-      </section> */}
-      {/* <section id="projects" className="snap-center">
+      </section>
+      <section id="projects" className="snap-center">
         <Projects data={projectsInfo?.projects} />
-      </section> */}
+      </section>
       <section id="contact" className="snap-center">
         <Contact />
       </section>
