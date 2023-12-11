64c37b9655ae4dcc78018ebf15dd7b5359b271fc
Hugo Tamayo <doctamayot@hotmail.com>
2023-12-10 19:23:58 -0500

    intento 1

--- components/About.tsx
+++ components/About.tsx
@@ -28,7 +28,7 @@ export default function About({ data }: any) {
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
-        src={urlFor(info?.heroImage).url()}
+        src={urlFor(info?.profilePic).url()}
         className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
       />
 
